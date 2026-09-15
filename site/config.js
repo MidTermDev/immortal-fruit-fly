2a2f0eeb3918f2c0161a36acb9c8768b84a708fc
midtermdev <217126910+MidTermDev@users.noreply.github.com>
2026-09-15 05:19:01 +0000

    Site: prefer RPC that serves event logs

--- site/config.js
+++ site/config.js
@@ -3,7 +3,7 @@
 window.FLY_CONFIG = {
   chainId: 56,
   chainName: 'BNB Smart Chain',
-  rpc: ['https://bsc-dataseed.bnbchain.org', 'https://bsc-dataseed1.binance.org', 'https://bsc-rpc.publicnode.com'],
+  rpc: ['https://bsc-rpc.publicnode.com', 'https://bsc-dataseed.bnbchain.org', 'https://bsc-dataseed1.binance.org'],
   explorer: 'https://bscscan.com',
   token: "0x23791aa3b031659b593cf141a2bc76b0ad657777",   // Immortal Fruit Flies ($FLY)
   brain: "0x32D28e97b50f5978eb51d7608492CC7221b01f63",   // FlyBrain (genesis fly)
